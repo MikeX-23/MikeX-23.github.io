f15e68da82c0ed75555fa59f5108a4ef85f45207
Mike X <mikex_23@yahoo.com>
2017-11-09 02:21:16 -0500

    changes to index

---
+++ M6.js
@@ -0,0 +1,3 @@
+function myFunction() {
+		document.getElementById("serv").innerHTML = "Some services we provide include: <br> <br> - Goals Assessment <br> - Metrics and Measurement <br> - Branding and Identity <br> - Custom Content <br> - Platform Strategy";
+	}
